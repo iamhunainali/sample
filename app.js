36ef73b5d60406847ab69bf39b4f11730b05188e
iamhunainali <iamhunainbadsha@gmail.com>
2026-02-18 07:41:46 -0800

    fresh

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('', (req, res) = {
+  res.send('Hello from GitHub → AWS via CICD!');
+});
+
+const port = process.env.PORT  3000;
+app.listen(port, () = {
+  console.log(`App running on port ${port}`);
+});
