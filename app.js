1e19129c9f310b6aff9e09ed7a8012f51e45ede7
iamhunainali <iamhunainbadsha@gmail.com>
2026-02-18 07:47:59 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) = {
-  res.send('Hello from GitHub → AWS via CICD!');
+  res.send('Hello from GitHub → AWS via CI/CD!');
 });
 
 const port = process.env.PORT  3000;
